be26f8b9a91323cb931adc48d345c3e555e87141
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2024-02-29 15:21:30 +0100

    Aggiornato il percorso di origine per l'API a "/api/request"

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
         {
             // matching all API routes
-            source: "/api/:path*",
+            source: "/api/request",
             headers: [
                 { key: "Access-Control-Allow-Credentials", value: "true" },
                 { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
